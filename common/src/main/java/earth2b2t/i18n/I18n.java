1885ff08b791210e0236d702e233979b3e019b0f
Toshimichi0915 <main@toshimichi.net>
2021-10-22 19:04:29 +0900

    Add I18n#plain

--- common/src/main/java/earth2b2t/i18n/I18n.java
+++ common/src/main/java/earth2b2t/i18n/I18n.java
@@ -10,6 +10,16 @@ import java.util.UUID;
 public interface I18n {
 
     /**
+     * Returns a message based on player's language.
+     * Location system will be disabled with this method.
+     * @param uuid {@link UUID} of the player
+     * @param key  translation key
+     * @param args arguments used for placeholders, in order.
+     * @return Message based on player's language
+     */
+    String plain(UUID uuid, String key, Object... args);
+
+    /**
      * Sends a message based on player's language.
      *
      * @param uuid {@link UUID} of the player
@@ -17,6 +27,11 @@ public interface I18n {
      * @param args arguments used for placeholders, in order.
      */
     void print(UUID uuid, String key, Object... args);
+
+    /* Bukkit-specific implementation */
+    default String plain(Player player, String key, Object... args) {
+        return plain(player.getUniqueId(), key, args);
+    }
 
     /* Bukkit-specific implementation */
     default void print(Player player, String key, Object... args) {
