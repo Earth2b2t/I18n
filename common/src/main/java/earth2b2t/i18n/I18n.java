fde9ed01ff20b9b03318444ed37c98a0dc6257fb
Toshimichi0915 <main@toshimichi.net>
2021-10-22 20:25:12 +0900

    Change I18n Bukkit implementations

--- common/src/main/java/earth2b2t/i18n/I18n.java
+++ common/src/main/java/earth2b2t/i18n/I18n.java
@@ -1,5 +1,6 @@
 package earth2b2t.i18n;
 
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import java.util.UUID;
@@ -40,12 +41,20 @@ public interface I18n {
     void print(UUID uuid, String key, Object... args);
 
     /* Bukkit-specific implementation */
-    default String plain(Player player, String key, Object... args) {
-        return plain(player.getUniqueId(), key, args);
+    default String plain(CommandSender sender, String key, Object... args) {
+        if (sender instanceof Player) {
+            return plain(((Player) sender).getUniqueId(), key, args);
+        } else {
+            return plain(key, args);
+        }
     }
 
     /* Bukkit-specific implementation */
-    default void print(Player player, String key, Object... args) {
-        print(player.getUniqueId(), key, args);
+    default void print(CommandSender sender, String key, Object... args) {
+        if (sender instanceof Player) {
+            print(((Player) sender).getUniqueId(), key, args);
+        } else {
+            sender.sendMessage(plain(key, args));
+        }
     }
 }
