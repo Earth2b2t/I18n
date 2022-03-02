9b4b668817e8524d6a4fdf13656da3f1ae7179c8
Toshimichi0915 <main@toshimichi.net>
2022-03-03 02:19:06 +0900

    Move some packages

--- bukkit/src/main/java/earth2b2t/BukkitI18n.java
+++ bukkit/src/main/java/earth2b2t/BukkitI18n.java
@@ -1,26 +1,29 @@
-package earth2b2t.bungeecord;
+package earth2b2t;
 
+import earth2b2t.bukkit.CachedLanguageProvider;
+import earth2b2t.bukkit.ChatLocation;
+import earth2b2t.bukkit.OptionLanguageProvider;
+import earth2b2t.bukkit.SubTitleLocation;
+import earth2b2t.bukkit.TitleLocation;
 import earth2b2t.i18n.FileLanguageProvider;
 import earth2b2t.i18n.LanguageProvider;
 import earth2b2t.i18n.Location;
 import earth2b2t.i18n.PropertiesI18n;
 import earth2b2t.i18n.RemoteLanguageProviderAdapter;
-import net.md_5.bungee.api.ProxyServer;
-import net.md_5.bungee.api.plugin.Plugin;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.UncheckedIOException;
-import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.WeakHashMap;
 
-public class BungeeCordI18n extends PropertiesI18n {
+public class BukkitI18n extends PropertiesI18n {
 
     /* This has to be WeakHashMap, as memory-leak issues might arise on plugin unloading */
-    private static final WeakHashMap<Plugin, BungeeCordI18n> cached = new WeakHashMap<>();
+    private static final WeakHashMap<Plugin, BukkitI18n> cached = new WeakHashMap<>();
     private static final Collection<Location> LOCATIONS;
     private static final Location DEFAULT_LOCATION = new ChatLocation();
 
@@ -32,35 +35,24 @@ public class BungeeCordI18n extends PropertiesI18n {
 
     private final Plugin plugin;
 
-    private BungeeCordI18n(Plugin plugin) throws IOException {
+    private BukkitI18n(Plugin plugin) throws IOException {
         super(plugin.getDataFolder(), plugin.getClass(), LOCATIONS, DEFAULT_LOCATION);
         this.plugin = plugin;
     }
 
-    public static BungeeCordI18n get(Class<?> c) {
-        File pluginFile;
-        try {
-            pluginFile = new File(c.getProtectionDomain().getCodeSource().getLocation().toURI());
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
-
-        Plugin plugin = ProxyServer.getInstance().getPluginManager().getPlugins().stream()
-                .filter(it -> it.getFile().equals(pluginFile))
-                .findAny()
-                .orElse(null);
-
+    public static BukkitI18n get(Class<?> c) {
+        JavaPlugin plugin = JavaPlugin.getProvidingPlugin(c);
         if (plugin == null) {
             throw new IllegalArgumentException("Provided class is not a part of any plugin: " + c.getCanonicalName());
         }
         return get(plugin);
     }
 
-    public static BungeeCordI18n get(Plugin plugin) {
-        BungeeCordI18n i18n = cached.get(plugin);
+    public static BukkitI18n get(Plugin plugin) {
+        BukkitI18n i18n = cached.get(plugin);
         if (i18n != null) return i18n;
         try {
-            i18n = new BungeeCordI18n(plugin);
+            i18n = new BukkitI18n(plugin);
         } catch (IOException e) {
             throw new UncheckedIOException(e);
         }
--- bungeecord/src/main/java/earth2b2t/BungeeCordI18n.java
+++ bungeecord/src/main/java/earth2b2t/BungeeCordI18n.java
@@ -1,24 +1,31 @@
-package earth2b2t.bukkit;
+package earth2b2t;
 
+import earth2b2t.bungeecord.CachedLanguageProvider;
+import earth2b2t.bungeecord.ChatLocation;
+import earth2b2t.bungeecord.OptionLanguageProvider;
+import earth2b2t.bungeecord.SubTitleLocation;
+import earth2b2t.bungeecord.TitleLocation;
 import earth2b2t.i18n.FileLanguageProvider;
 import earth2b2t.i18n.LanguageProvider;
 import earth2b2t.i18n.Location;
 import earth2b2t.i18n.PropertiesI18n;
 import earth2b2t.i18n.RemoteLanguageProviderAdapter;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.java.JavaPlugin;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.plugin.Plugin;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.UncheckedIOException;
+import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.WeakHashMap;
 
-public class BukkitI18n extends PropertiesI18n {
+public class BungeeCordI18n extends PropertiesI18n {
 
     /* This has to be WeakHashMap, as memory-leak issues might arise on plugin unloading */
-    private static final WeakHashMap<Plugin, BukkitI18n> cached = new WeakHashMap<>();
+    private static final WeakHashMap<Plugin, BungeeCordI18n> cached = new WeakHashMap<>();
     private static final Collection<Location> LOCATIONS;
     private static final Location DEFAULT_LOCATION = new ChatLocation();
 
@@ -30,24 +37,35 @@ public class BukkitI18n extends PropertiesI18n {
 
     private final Plugin plugin;
 
-    private BukkitI18n(Plugin plugin) throws IOException {
+    private BungeeCordI18n(Plugin plugin) throws IOException {
         super(plugin.getDataFolder(), plugin.getClass(), LOCATIONS, DEFAULT_LOCATION);
         this.plugin = plugin;
     }
 
-    public static BukkitI18n get(Class<?> c) {
-        JavaPlugin plugin = JavaPlugin.getProvidingPlugin(c);
+    public static BungeeCordI18n get(Class<?> c) {
+        File pluginFile;
+        try {
+            pluginFile = new File(c.getProtectionDomain().getCodeSource().getLocation().toURI());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        Plugin plugin = ProxyServer.getInstance().getPluginManager().getPlugins().stream()
+                .filter(it -> it.getFile().equals(pluginFile))
+                .findAny()
+                .orElse(null);
+
         if (plugin == null) {
             throw new IllegalArgumentException("Provided class is not a part of any plugin: " + c.getCanonicalName());
         }
         return get(plugin);
     }
 
-    public static BukkitI18n get(Plugin plugin) {
-        BukkitI18n i18n = cached.get(plugin);
+    public static BungeeCordI18n get(Plugin plugin) {
+        BungeeCordI18n i18n = cached.get(plugin);
         if (i18n != null) return i18n;
         try {
-            i18n = new BukkitI18n(plugin);
+            i18n = new BungeeCordI18n(plugin);
         } catch (IOException e) {
             throw new UncheckedIOException(e);
         }
